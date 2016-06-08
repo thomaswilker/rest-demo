d29e62d7349efe958f53342b0e38286eae492d7c
thomaswilker <thomaswilker62@gmail.com>
2016-06-08 21:35:01 +0200

    findByTitleContainingIgnoreCase added

--- src/main/java/rest/demo/repository/BlogPostRepository.java
+++ src/main/java/rest/demo/repository/BlogPostRepository.java
@@ -1,8 +1,16 @@
 package rest.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.Description;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import rest.demo.model.BlogPost;
 
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long> {
+	
+	@RestResource(path="findyByTitle", rel="findyByTitle", exported=true, description=@Description("Beschreibung"))
+	public List<BlogPost> findByTitleContainingIgnoreCase(@Param("title") String title);
 }
